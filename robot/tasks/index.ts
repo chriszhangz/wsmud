710d0b345d74a5d51c0e94fd3912a60c6b940af1
chriszhangz <apollo_cc@sina.com>
2018-07-26 17:35:28 -0700

    update

--- robot/tasks/index.ts
+++ robot/tasks/index.ts
@@ -4,6 +4,7 @@ import { BadiTask } from "./badi";
 import { ChrisTask } from "./chris";
 import { ConnectTask } from "./connect";
 import { MapTask } from "./map";
-export { SmTask, AnswerTask, BadiTask,ChrisTask, ConnectTask, MapTask }
+import { MineTask } from "./mine";
+export { SmTask, AnswerTask, BadiTask,ChrisTask, ConnectTask, MapTask, MineTask }
 
 
--- robot/tasks/mine.ts
+++ robot/tasks/mine.ts
@@ -1,9 +1,29 @@
 import { Task } from "../task";
+import { Session } from "../../core";
+import { UserConfig } from "../interface";
+import {Promise} from "bluebird";
 
 export class MineTask extends Task {
 
     constructor() {
         super();
         this.priority = 100;
+    }
+    private cmds: string[] = ["jh fam 1 start","go north"];
+    async start(session: Session, config: UserConfig) {
+        for (let i = 0; i < this.cmds.length; i++) {
+            await session.sendAsync(this.cmds[i]);
+            await Promise.delay(1000);
+            let roomData = session.world.room;
+            console.log(roomData.name);
+            await Promise.delay(1000);
+            let items = session.world.items;
+            for(const item in items){
+                if(items[item].p!=1)
+                console.log(items[item].name);
+            }
+        }
+        this.priority = -1;
+
     }
 }
