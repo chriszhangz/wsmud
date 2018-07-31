346a2572a758bd913d004043561b4c5f2f169792
chriszhangz <apollo_cc@sina.com>
2018-07-30 21:07:54 -0700

    update

--- robot/tasks/chris.ts
+++ robot/tasks/chris.ts
@@ -49,16 +49,16 @@ export class ChrisTask extends Task {
                 time = time / 1000;
                 var mins = Math.floor(time / 60);
                 var secs = Math.floor(time % 60);
-                return `${ch} 上一个BOSS出现在${mins}分${secs}秒以前`;
+                return `${ch} 🐾上一个BOSS出现在${mins}分${secs}秒以前`;
             }
             if(nd.getHours()===lastHour){
                 var time = new Date().getTime() - lastbook.getTime();
                 time = time / 1000;
                 var mins = Math.floor(time / 60);
                 var secs = Math.floor(time % 60);
-                return `${ch} ${hour}点BOSS已经出现在${mins}分${secs}秒以前`;
+                return `${ch} 🐾${hour}点BOSS已经出现在${mins}分${secs}秒以前`;
             }else{
-                return `${ch} ${hour}点BOSS还未刷新，请耐心等待~`;
+                return `${ch} 😟${hour}点BOSS还未刷新，请耐心等待~`;
             }
             //return `${mins}分${secs}秒`;
         }   
@@ -130,48 +130,48 @@ export class ChrisTask extends Task {
                     if (content.indexOf('白如盈') >=0 && content.indexOf('你好')>=0) {
                         var userName = data.name;
                         if(data.name==='燧人氏'){
-                            await session.sendAsync(`${ch} 燧大侠~ 您好^^！`);
+                            await session.sendAsync(`${ch} 😍燧大侠~ 您好^^！`);
                         }else if(data.name==='半俗'){
-                            await session.sendAsync(`${ch} 俗~ 今天玫瑰送了没^^？`);
+                            await session.sendAsync(`${ch} 😊俗~ 今天玫瑰送了没^^？`);
                         }else if(data.name==='法王孙笑川'){
-                            await session.sendAsync(`${ch} 新人导师法王孙笑川~ 您好^^！`);
+                            await session.sendAsync(`${ch} ‍🧙‍♂️新人导师法王孙笑川~ 您好^^！`);
                         }else if(data.name==='讓叁招'){
                             await session.sendAsync(`${ch} 阿三~ 你好^^！`);
                         }else if(data.name==='诸葛擎'){
-                            await session.sendAsync(`${ch} 萌新导师诸葛擎~ 您好^^！`);
+                            await session.sendAsync(`${ch} ‍‍‍‍‍🧙‍♂️萌新导师诸葛擎~ 您好^^！`);
                         }else if(data.name==='关心'){
-                            await session.sendAsync(`${ch} 关心小姐姐好^^！`);
+                            await session.sendAsync(`${ch} 😄关心小姐姐好^^！`);
                         }else if(data.name==='轩辕天宇'){
-                            await session.sendAsync(`${ch} 天宇小姐姐好^^！`);
+                            await session.sendAsync(`${ch} 😄天宇小姐姐好^^！`);
                         }else if(data.lv===5){
-                            await session.sendAsync(`${ch} 哇武。。。武帝您好, ${userName}！`);
+                            await session.sendAsync(`${ch} 哇武。。。武帝您好😻, ${userName}！`);
                         }else if(data.lv===4){
-                            await session.sendAsync(`${ch} 您好武圣大佬, ${userName}！`);
+                            await session.sendAsync(`${ch} 您好武圣大佬😊, ${userName}！`);
                         }else if(data.lv===3){
-                            await session.sendAsync(`${ch} 您好宗师前辈, ${userName}！`);
+                            await session.sendAsync(`${ch} 您好宗师前辈😄, ${userName}！`);
                         }else{
                             await session.sendAsync(`${ch} 您好, ${userName}！`);
                         }
                         lastchat = new Date();
-                    }else if (content.indexOf('白如盈') >=0&& content.indexOf('什么')>=0&&( content.indexOf('用')>=0||content.indexOf('能')>=0)) {
+                    }else if (content.indexOf('白如盈') >=0&& content.indexOf('什么')>=0&&( content.indexOf('用')>=0||content.indexOf('能')>=0||content.indexOf('升级')>=0)) {
                         var userName = data.name;
                         await session.sendAsync(`${ch} 您好${userName}，我已升级2.0版，能报boss时间以及具体位置（华山部分地图除外）。如有需要请联系我的主人谢谢。`);
                         lastchat = new Date();
                     }else if (content.indexOf('白如盈') >=0&& content.indexOf('主人')>=0&& content.indexOf('谁')>=0) {
                         var userName = data.name;
-                        await session.sendAsync(`${ch} 您好${userName}，我的主人是咬人的豆包。`);
+                        await session.sendAsync(`${ch} 😊您好${userName}，我的主人是咬人的豆包。`);
                         lastchat = new Date();
                     }else if (content.indexOf('白如盈') >=0&& content.indexOf('是谁')>=0) {
                         var userName = data.name;
-                        await session.sendAsync(`${ch} 您好${userName}，我是豆包的机器人。`);
-                        lastchat = new Date();
-                    }else if (content.indexOf('白如盈') >=0&& content.indexOf('在哪儿')>=0) {
+                        await session.sendAsync(`${ch} 😊您好${userName}，我是豆包的机器人。`);
+                        lastchat = new Date();{
                         var userName = data.name;
-                        await session.sendAsync(`${ch} 您好${userName}，我在挖矿^^。`);
+                    }else if (content.indexOf('白如盈') >=0&& content.indexOf('在哪儿')>=0) { 
+                        await session.sendAsync(`${ch} 😄您好${userName}，我在挖矿^^。`);
                         lastchat = new Date();
-                    }else if (content.indexOf('白如盈') >=0&& content.indexOf('爱你')>=0) {
+                    }else if (content.indexOf('白如盈') >=0&& content.indexOf('不') <0 && (content.indexOf('爱你')>=0||content.indexOf('美')>=0||content.indexOf('漂亮')>=0||content.indexOf('好看')>=0)) {
                         var userName = data.name;
-                        await session.sendAsync(`${ch} *咯咯`);
+                        await session.sendAsync(`${ch} *脸红`);
                         lastchat = new Date();
                     }else if (content.indexOf('白如盈') >=0 &&( content.indexOf('傻')>=0||content.indexOf('笨')>=0||content.indexOf('贱')>=0||content.indexOf('蠢')>=0||content.indexOf('白痴')>=0||content.indexOf('弱智')>=0)) {
                         var userName = data.name;
@@ -180,11 +180,11 @@ export class ChrisTask extends Task {
                     }else if (content.indexOf('白如盈') >=0) {
                         var userName = data.name;
                         if(data.lv===5){
-                            await session.sendAsync(`${ch} 哇武。。。武帝${userName}您好, 有何吩咐？`);
+                            await session.sendAsync(`${ch} 😻哇武。。。武帝${userName}您好, 有何吩咐？`);
                         }else if(data.lv===4){
-                            await session.sendAsync(`${ch} 您好武圣大佬${userName}, 有何指示？`);
+                            await session.sendAsync(`${ch} 😊您好武圣大佬${userName}, 有何指示？`);
                         }else if(data.lv===3){
-                            await session.sendAsync(`${ch} 您好宗师前辈${userName}, 需要我做什么？`);
+                            await session.sendAsync(`${ch} 😄您好宗师前辈${userName}, 需要我做什么？`);
                         }else{
                             await session.sendAsync(`${ch} 您叫我有事么, ${userName}?`);
                         }
@@ -197,7 +197,7 @@ export class ChrisTask extends Task {
                                }
                             await session.sendAsync(`${GetChinaTime()}`+positions);
                         }else{
-                            await session.sendAsync(`${ch} 抱歉，我刚升级完毕,将等待下一个BOSS出现后开始计时。`);
+                            await session.sendAsync(`${ch} 😉抱歉，我刚升级完毕,将等待下一个BOSS出现后开始计时。`);
                         }
                         lastchat = new Date();
                     }
