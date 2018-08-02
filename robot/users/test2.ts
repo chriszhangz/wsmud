f83872f7c6429a896bc462db9a802c56e172b814
chriszhangz <apollo_cc@sina.com>
2018-08-01 23:32:38 -0700

    update

--- robot/users/test2.ts
+++ robot/users/test2.ts
@@ -2,10 +2,10 @@ import { UserConfig } from "../interface";
 import * as tasks from "../tasks";
 export var test2: UserConfig = {
     key: "test2",
-    name: "戚镰剑",
+    name: "司徒昌昊",
     server: 2,
     account: {
-        code: "apollozz1",
+        code: "apollozz",
         pwd: "1982525"
     },
     tasks: [
