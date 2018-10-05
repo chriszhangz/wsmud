ed225ca3afc5b48869f7a87c4f44ca282b826898
chriszhangz <apollo_cc@sina.com>
2018-10-04 20:06:25 -0700

    5

--- robot/cron2.ts
+++ robot/cron2.ts
@@ -10,19 +10,22 @@ import { UserConfig } from "./interface";
 import { TaskSchedule } from "./task-schedule";
 import * as tasks from "./tasks";
 
+const ONE_DAY_MS = 86400000;
+
+const firstChiefTime = new Date(2018,9,29).getTime();
 async function start() {
     async function callback() {
         //const config = getConfig();
         let config: UserConfig = {
             key: "test4",
-            name: "戚镰剑",
+            name: "咬人的豆包",
             server: 2,
             account: {
-                code: "apollozz1",
+                code: "apollozzz",
                 pwd: "1982525"
             },
             tasks: [
-                new tasks.ShimenTask("jh fam 3 start", "高根明", "956z2902a39", new Date(2018, 4, 21))
+                new tasks.ShouxiTask("jh fam 5 start;go north", "苏星河", "8wdo2ae4caf", new Date(2018,9,28))
             ]
         }
         console.log("begin ......................................" + config.name);
@@ -35,13 +38,38 @@ async function start() {
 
         console.log("ALL JOBS DONE !!!!!!!!!!!!!!!!!!!!!!!!");
     }
+    
     //var rule = new RecurrenceRule();
     //var rule = new schedule.RecurrenceRule();
     var CronJob = require('cron').CronJob;
     new CronJob('55 0 14 * * *', async function() {
-        await callback();
+        const today = new Date();
+        var days = (today.getTime() - firstChiefTime) / ONE_DAY_MS;
+        console.log(`days:${days}`);
+        var offset = (days & 1) == 0;
+
+        console.log(`${today} ${offset ? 'is' : 'is not'} chief date`);
+        if(!offset){
+            //console.log(`do 20 clock job`);
+            await callback();
+        }
       }, null, true, 'America/Los_Angeles');
 
+      new CronJob('5 0 5 * * *', async function() {
+        const today = new Date();
+        var days = (today.getTime() - firstChiefTime) / ONE_DAY_MS;
+        console.log(`days:${days}`);
+        var offset = (days & 1) == 0;
+        console.log(`${today} ${offset ? 'is' : 'is not'} chief date`);
+        if(offset){
+            //console.log(`do 5 clock job`);
+            await callback();
+        }
+      }, null, true, 'America/Los_Angeles');      
+
+      
 }
 
 start();
+
+
